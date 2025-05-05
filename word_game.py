664a1f97a3a1e8c2a7f79f2826b4916587c1b1a8
ohidul225 <251-15-917@diu.edu.bd>
2025-05-05 22:08:12 +0600

    Story Add

--- word_game.py
+++ word_game.py
@@ -1,19 +1,14 @@
-# A Silly Adventure Story Game
+#A story game
 
-print("Welcome to the Silly Adventure Story Game!")
+adjective1 = input("Enter an adjective (Description): ")
+noun1 = input("Enter a noun (person ,place, thing): ")
+adjective2 = input("Enter another adjective (Description): ")
+animal = input("Enter an animal: ")
+verb1 = input("Enter a verb ending with 'ing': ")
+noun2 = input("Enter another noun(person ,place, thing): ")
+adjective3 = input("Enter one more adjective (Description): ")
 
-adjective1 = input("Enter a ridiculously silly adjective: ")
-noun1 = input("Enter a weird place (e.g., 'haunted donut shop'): ")
-adjective2 = input("Enter a funky adjective: ")
-animal = input("Name the most absurd animal you can think of: ")
-verb1 = input("Enter a funny verb ending in 'ing' (e.g., 'moonwalking'): ")
-noun2 = input("Enter the most random object you own: ")
-adjective3 = input("Enter a strange adjective: ")
-
-print("\nHere's your silly story:\n")
-print(f"One {adjective1} morning, I woke up inside a {noun1}.")
-print(f"The sky was {adjective2} and smelled faintly of tacos, which meant only one thing...")
-print(f"The legendary {animal} was near!")
-print(f"While {verb1} to get a better look, I tripped over my {noun2} and fell into a puddle of glitter.")
-print(f"Thankfully, a {adjective3} squirrel wearing sunglasses helped me up and offered me a banana.")
-print("And that's how I became ruler of the Land of Nonsense!")
+print(f"One {adjective1} day, I decided to visit the {noun1}.")
+print(f"The weather was {adjective2} and I couldn't wait to see the {animal}.")
+print(f"While {verb1}, I accidentally dropped my {noun2}!")
+print(f"Luckily, a {adjective3} guide helped me find it.")
